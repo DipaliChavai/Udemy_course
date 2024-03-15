cdad4ac85ad377bcfc32a080b55468dfb514cab6
Dipali Chavai <10707@gsource.com>
2024-03-15 18:34:37 +0530

    authors_api_project

--- core_apps/profiles/models.py
+++ core_apps/profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+from core_apps.common.models import TimeStampModel
 
-# Create your models here.
+
+User = get_user_model()
+
+class Profile(TimeStampModel):
+    class Gender(models.TextChoices):
+        MALE = "M", _("Male"),
+        
+
